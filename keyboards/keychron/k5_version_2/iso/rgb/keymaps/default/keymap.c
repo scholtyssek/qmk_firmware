150739711bcb17a1a9abbd7bb2bb3ec059dff78b
Marco Scholtyssek <marco@scholtyssek.org>
2026-03-26 16:21:30 +0100

    keymap updated

--- keyboards/keychron/k5_version_2/iso/rgb/keymaps/default/keymap.c
+++ keyboards/keychron/k5_version_2/iso/rgb/keymaps/default/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,  KC_PSCR,  KC_CTANA, UG_NEXT,  KC_CALC, KC_LPAD,   KC_F15,   KC_F16,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     LT(NUMBERS,KC_E),     KC_R,     KC_T,     KC_Y,     KC_U,     LT(NUMBERS,KC_I),     LT(WIN_BASE,KC_O),     KC_P,     KC_LBRC,  KC_RBRC,  KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
-        MO(WIN_BASE),  MT(MOD_LSFT,KC_A),  MT(MOD_LALT,KC_S),     MT(MOD_LCTL,KC_D),     LT(MAC_FN,KC_F),    KC_G,     KC_H,     LT(MAC_FN,KC_J),    MT(MOD_LCTL,KC_K),     MT(MOD_LALT,KC_L),     MT(MOD_LSFT,KC_SCLN),  KC_QUOT,  KC_NUHS,  KC_ENT,                                KC_P4,    KC_P5,    KC_P6,
+        MO(WIN_BASE),  MT(MOD_LSFT,KC_A),  MT(MOD_LALT,KC_S),     MT(MOD_LCTL,KC_D),     LT(MAC_FN,KC_F),    KC_G,     KC_H,     LT(MAC_FN,KC_J),    MT(MOD_LCTL,KC_K),     MT(MOD_LALT,KC_L),     MT(MOD_LSFT,KC_SCLN),  KC_QUOT,  KC_NUHS,  KC_ENT,  KC_P4,    KC_P5,    KC_P6,
         KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,           KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_LWIN, MO(WIN_FN),MO(NUMBERS), KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT          ),
 
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,  KC_PSCR,  KC_CTANA, UG_NEXT,  KC_F13,   KC_F14,   KC_F15,   KC_F16,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,   DE_AT,     KC_GRV,   DE_LABK,  DE_RABK,  DE_HASH,     DE_QUES,  DE_LBRC,  DE_RBRC,  DE_EXLM,  DE_DLR,   DE_SLSH,  KC_RBRC,  KC_DEL,  KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
-        KC_CAPS,  KC_A,     DE_PERC,     KC_DEL,     KC_BSPC,     KC_ENT,     DE_DQUO,  DE_LPRN,  DE_RPRN,  DE_QUOT,  DE_ASTR,  DE_BSLS,  KC_NUHS,  KC_ENT,                                KC_P4,    KC_P5,    KC_P6,
+        KC_CAPS,  KC_A,     DE_PERC,     KC_DEL,     KC_BSPC,     _______,     DE_DQUO,  DE_LPRN,  DE_RPRN,  DE_QUOT,  DE_ASTR,  DE_BSLS,  KC_NUHS,  KC_ENT,                                KC_P4,    KC_P5,    KC_P6,
         KC_LSFT,  KC_NUBS,  DE_GRV,     DE_ACUT,     DE_TILD,     DE_PIPE,     KC_B,     DE_EQL,     DE_LCBR,     DE_RCBR,  DE_SLSH,   DE_AMPR,            KC_RSFT,           KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN, MO(WIN_FN),KC_RCTL, KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT          ),
 
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_TAB,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,           KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_LEFT,     KC_DOWN,     KC_UP,     KC_RGHT,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,                                KC_P4,    KC_P5,    KC_P6,
-        KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,           KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
+        KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_HOME,     KC_PGDN,     KC_PGUP,  KC_END,   KC_SLSH,   KC_RSFT,           KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN, MO(WIN_FN),KC_RCTL, KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT          ),
 
     [WIN_FN] = LAYOUT_109_iso(
