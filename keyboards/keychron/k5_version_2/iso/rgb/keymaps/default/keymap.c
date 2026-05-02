c46f2742f6919b77617f8e0ece66d3b2f90bc364
Marco Scholtyssek <scholtyssek@users.noreply.github.com>
2026-05-03 01:58:19 +0200

    percent added

--- keyboards/keychron/k5_version_2/iso/rgb/keymaps/default/keymap.c
+++ keyboards/keychron/k5_version_2/iso/rgb/keymaps/default/keymap.c
@@ -45,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_FN] = LAYOUT_109_iso(
         KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,  KC_PSCR,  KC_CTANA, UG_NEXT,  KC_F13,   KC_F14,   KC_F15,   KC_F16,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
-        KC_TAB,   DE_AT,     KC_GRV,   DE_LABK,  DE_RABK,  DE_HASH,     DE_QUES,  DE_LBRC,  DE_RBRC,  DE_EXLM,  DE_DLR,   DE_SLSH,  KC_RBRC,  KC_DEL,  KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
+        KC_TAB,   DE_AT,     KC_GRV,   DE_LABK,  DE_RABK,  DE_HASH,     DE_QUES,  DE_LBRC,  DE_RBRC,  DE_EXLM,  DE_DLR,   DE_PERC,  KC_RBRC,  KC_DEL,  KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
         KC_CAPS,  KC_A,     DE_PERC,     KC_DEL,     KC_BSPC,     _______,     DE_DQUO,  DE_LPRN,  DE_RPRN,  DE_QUOT,  DE_ASTR,  DE_BSLS,  KC_NUHS,  KC_ENT,                                KC_P4,    KC_P5,    KC_P6,
-        KC_LSFT,  KC_NUBS,  DE_GRV,     DE_ACUT,     DE_TILD,     DE_PIPE,     KC_B,     DE_EQL,     DE_LCBR,     DE_RCBR,  DE_SLSH,   DE_AMPR,            KC_RSFT,           KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
+        KC_LSFT,  KC_NUBS,  DE_GRV,     DE_ACUT,     DE_TILD,     DE_PIPE,     DE_PERC,     DE_EQL,     DE_LCBR,     DE_RCBR,  DE_SLSH,   DE_AMPR,            KC_RSFT,           KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN, MO(WIN_FN),KC_RCTL, KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT          ),
 
     [WIN_BASE] = LAYOUT_109_iso(
